477523867a2ac7d4b000e140fafe53343ac1156b
sunitj <sunitj@umich.edu>
2014-07-15 10:57:04 -0400

    Tue Jul 15 10:57:04 EDT 2014

--- AssemblyTools/gcSkew.pl
+++ AssemblyTools/gcSkew.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 =head1 DESCRIPTION
 
-	gcSkew.pl v0.0.2	-	takes a fasta formatted nucleotide file and calculates GC skew [(C-G)/(C+G)]*100 content for the whole sequence in sliding windows. You can specify the window length, and amount of overlap between successive windows.
+	gcSkew.pl	-	takes a fasta formatted nucleotide file and calculates GC skew [(C-G)/(C+G)]*100 content for the whole sequence in sliding windows. You can specify the window length, and amount of overlap between successive windows.
 
 =head1 USAGE
 
@@ -28,18 +28,20 @@ use strict;
 use Getopt::Long;
 
 my $help;
-my $version=0.0.3;
+my $version=0.0.8;
 my $fasta;
 my $out=$$.".gcskew";
-my $window=4;
+my $window=100;
 my $overlap=0;
+my $minLen=1000;
 
 GetOptions(
 	'f|fasta=s'=>\$fasta,
 	'w|window:i'=>\$window,
 	'o|out:s'=>\$out,
 	'overlap:i'=>\$overlap,
-	'h|help'=>\sub{system('perldoc', $0); exit;},
+	'l|min_len:i'=>\$minLen,
+	'h|help'=>sub{system('perldoc', $0); exit;},
 );
 
 die "Overlap has to be less than the Window size\n" if ($overlap >= $window);
@@ -53,7 +55,7 @@ print "Overlap:\t".$overlap."\n";
 
 open(FASTA, $fasta)|| die $!;
 open(OUT, ">".$out)|| die $!;
-print OUT "# Header\tStartPos\tStopPos\tGC-Skew(\%)\n";
+print OUT "# Header\tStartPos\tStopPos\tGC-Skew\tGC(\%)\n";
 $/=">";
 my $totalWindows;
 my $totalSequences;
@@ -63,7 +65,7 @@ while(my $line=<FASTA>){
 
 	my($header,@seq)=split(/\n/, $line);
 	my $sequence=join("", @seq);
-
+	next unless (length($sequence) > $minLen);
 	$totalWindows+= gcSkew($header, $sequence);
 	$totalSequences++;
 }
@@ -90,13 +92,15 @@ sub gcSkew{
 
 			my $skew=0;
 			if (($C + $G) > 0){
-				$skew= (($C - $G)/($C + $G))* 100;
+				$skew= (($C - $G)/($C + $G))*100;
 			}
-			my $GCSkew = sprintf( "%.4f", $skew );
-			print OUT $header."\t".$start."\t".($start+$step)."\t".$GCSkew."\n";
+			my $gc=(($C+$G)/$window)*100;
+			my $GC = sprintf( "%.2f", $gc );
+			my $GCSkew = sprintf( "%.2f", $skew );
+			print OUT $header."\t".$start."\t".($start+$step)."\t".$GCSkew."\t".$GC."\n";
 		}
 		else{
-			print OUT $header."\t".$start."\t".$seqLen."\t0.0000\n";
+			print OUT $header."\t".$start."\t".$seqLen."\t0.00\t0.00\n";
 		}
 		$windows++;
 	}
