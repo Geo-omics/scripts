98f103a3ff62b37a5b329ab480187024957038a3
Sunit Jain <sunit@secondgenome.com>
2016-06-02 11:56:58 -0700

    June 2016 Updates

--- crawler.pl
+++ crawler.pl
@@ -2,17 +2,19 @@
 
 =head1 DESCRIPTION
 
-	crawler.pl
+crawler.pl	-	Checks the 'content.list' file for any new script that may have been created in sandbox. Copies the script to the appropriate directory. Maintains and updates these scripts and README for each category of scripts.
 
 =head1 USAGE
 
-	perl crawler.pl
+	perl crawler.pl -list ~/src/sandbox/contents.list -path2scripts ~/Github/scripts -path2sandbox ~/src/sandbox
 
 =head2 Options
 
-
+	-list	-l	<FILE>	A specially formatted file to categorize all the scripts. See the "contents.list" file, available in the same repo, for the description.
+	-path2scripts	-p	<PATH> Location of the folder that'll be committed/pushed to Github/BitBucket/Hg etc.
+	-path2sandbox	-s	<PATH>	Location of the folder where all the scripts reside.
 	-version -v	<BOOLEAN>	version of the current script
-	-help	-h	<BOOLEAN>	This message. press q to exit this screen.	
+	-help	-h	<BOOLEAN>	This message. press q to exit this screen.
 
 =head1 Author
 
@@ -28,12 +30,15 @@ use File::Path;
 use File::Copy;
 use FileHandle;
 
-my $version="crawler.pl\tv0.0.5b";
+my $version="crawler.pl\tv0.0.6";
 my $list="contents.list";
 my $path= "/geomicro/data1/COMMON/scripts/";
 my $sandbox="/geomicro/data1/COMMON/scripts/sandbox";
 
 GetOptions(
+	'p|path2scripts:s'=>\$path,
+	's|path2sandbox:s'=>\$sandbox,
+	'l|list:s'=>\$list,
 	'v|version'=>sub{print $version."\n"; exit;},
 	'h|help'=>sub{system('perldoc', $0); exit;},
 );
@@ -48,7 +53,7 @@ while(my $line=<LIST>){
 	next if $line=~ /^#/;
 	chomp $line;
 	next unless $line;
-	
+
 	if ($line=~ /\:$/){
 	# Folders
 		$line=~ s/\:$//;
@@ -60,7 +65,7 @@ while(my $line=<LIST>){
 			open($MD, ">".$contentsMD) || die "Can't create table of contents at: $contentsMD\n";
 			print $MD "## Script Descriptions\n";
 		}
-		
+
 		if (! -d $folder){
 			my $dir = eval{ mkpath($folder) };
 			die "Failed to create $line: $@\n" unless $dir;
@@ -74,7 +79,7 @@ while(my $line=<LIST>){
 			warn "'$file' must be a folder. Skipping...\n";
 			next;
 		}
-		
+
 		if(! -e $file){
 			warn "Not Found: $file\n";
 			next;
