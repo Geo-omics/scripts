d24f9e71332816eb1d384e1a8385c44b81bafb66
Robert <heinro@umich.edu>
2017-10-19 11:06:34 -0400

    Show available commands when `omics` is called without any options

--- lib/omics/__main__.py
+++ lib/omics/__main__.py
@@ -2,10 +2,11 @@
 Invoke omics scripts
 """
 import argparse
+from pathlib import Path
 import subprocess
 import sys
 
-from . import process_command_line
+from . import SCRIPT_PREFIX, process_command_line
 
 
 def main():
@@ -29,21 +30,69 @@ def main():
         help='The command to run.',
     )
     args = argp.parse_args()
-    cmd = process_command_line(args.command)
-    if args.dry_run:
-        print(*cmd)
-    else:
-        try:
-            p = subprocess.run(cmd)
-        except Exception as e:
-            if args.traceback:
-                raise
-            else:
-                print('{}: {}'.format(e.__class__.__name__, e),
-                      file=sys.stderr)
-                sys.exit(1)
+    if args.command:
+        cmd = process_command_line(args.command)
+        if args.dry_run:
+            print(*cmd)
         else:
-            sys.exit(p.returncode)
+            try:
+                p = subprocess.run(cmd)
+            except Exception as e:
+                if args.traceback:
+                    raise
+                else:
+                    print('{}: {}'.format(e.__class__.__name__, e),
+                          file=sys.stderr)
+                    sys.exit(1)
+            else:
+                sys.exit(p.returncode)
+    else:
+        subcmds = get_available_commands()
+        if subcmds:
+            print('Available commands:')
+            for i in subcmds:
+                print('  ', i)
+            print('Type `omics -h` or `omics <cmd> -h` to get help.')
+        else:
+            argp.print_help()
 
 
-main()
+def get_available_scripts():
+    """
+    Try to get the available omics commands
+
+    :return: List of paths for subcommand scripts.
+    :raises: In case of errors
+    """
+    p = subprocess.run(['which', 'omics'], stdout=subprocess.PIPE)
+    p.check_returncode()
+    path = Path(p.stdout.decode().strip()).parent
+    if path.is_dir():
+        return list(path.glob(SCRIPT_PREFIX + '*'))
+    else:
+        raise RuntimeError('Failed to determine directory containing omics '
+                           'executable: {}'.format(path))
+
+
+def get_available_commands():
+    """
+    Get list of available sub-commands
+
+    :return list: List of str names of sub-commands.
+                  List is empty in case of errors.
+    """
+    ret = []
+    try:
+        commands = get_available_scripts()
+    except:
+        pass
+    else:
+        for i in commands:
+            _, _, subcmd = i.name.partition('-')
+            if subcmd:
+                ret.append(subcmd)
+    return ret
+
+
+if __name__ == '__main__':
+    main()
