05867f66525cfd5b72fb619f2e9a751ba164f9a3
Robert <heinro@umich.edu>
2017-12-20 15:03:33 -0500

    [bins2fasta] Make script a proper module, add entry point under old name

--- lib/omics/bins2fasta.py
+++ lib/omics/bins2fasta.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 """
 Copy contigs into per-bin FASTA files
 
@@ -9,108 +8,110 @@ from pathlib import Path
 import sys
 
 
-argp = argparse.ArgumentParser(__doc__)
-argp.add_argument(
-    'clustering',
-    type=argparse.FileType(),
-    help='CONCOCT output file assigning contigs to clusters'
-)
-argp.add_argument(
-    'contigs',
-    type=argparse.FileType(),
-    help='Fasta formatted contigs file'
-)
-argp.add_argument(
-    '-s', '--suffix',
-    default='fna',
-    help='Suffix for output files, default is .fna'
-)
-argp.add_argument(
-    '-o', '--output-dir',
-    default='.',
-    help='Output directory, default is the current directory.'
-)
-argp.add_argument(
-    '-v', '--verbose',
-    action='store_true',
-    help='Print more info in case of errors'
-)
-args = argp.parse_args()
+def main():
+    argp = argparse.ArgumentParser(__doc__)
+    argp.add_argument(
+        'clustering',
+        type=argparse.FileType(),
+        help='CONCOCT output file assigning contigs to clusters'
+    )
+    argp.add_argument(
+        'contigs',
+        type=argparse.FileType(),
+        help='Fasta formatted contigs file'
+    )
+    argp.add_argument(
+        '-s', '--suffix',
+        default='fna',
+        help='Suffix for output files, default is .fna'
+    )
+    argp.add_argument(
+        '-o', '--output-dir',
+        default='.',
+        help='Output directory, default is the current directory.'
+    )
+    argp.add_argument(
+        '-v', '--verbose',
+        action='store_true',
+        help='Print more info in case of errors'
+    )
 
-outdir = Path(args.output_dir)
-outdir.mkdir(parents=True, exist_ok=True)
+    args = argp.parse_args()
 
-# load bin assignments
-contigs = {}
-bins = set()
-print('Reading bin assignment data... ', end='', flush=True)
-for line in args.clustering:
-    line = line.strip()
+    outdir = Path(args.output_dir)
+    outdir.mkdir(parents=True, exist_ok=True)
 
-    try:
-        contig_id, xbin = line.split(',')
-        xbin = int(xbin)
-    except Exception as e:
-        print('Failed to parse clustering file {}, offending line: {}: Error: '
-              '{}: {}'.format(args.clustering, line, e.__class__.__name__, e))
-        sys.exit(1)
+    # load bin assignments
+    contigs = {}
+    bins = set()
+    print('Reading bin assignment data... ', end='', flush=True)
+    for line in args.clustering:
+        line = line.strip()
 
-    contigs[contig_id] = xbin
-    bins.add(xbin)
-print('done')
-
-print('  Found {} contigs with bin assignments'.format(len(contigs)))
-print('  Found {} bins'.format(len(bins)))
-
-print('Copying contigs...', end='', flush=True)
-# get output file handles
-files = {}
-for i in bins:
-    suffix = args.suffix.lstrip('.')
-    fname = 'bin_{}.{}'.format(i, suffix)
-    files[i] = (outdir / fname).open('w')
-
-# write contigs
-contig_id = None
-num_not_binned = 0
-for line in args.contigs:
-    is_header = line.startswith('>')
-    if is_header:
-        # account previous contig
         try:
-            del contigs[contig_id]
-        except KeyError:
-            pass
-        contig_id, _, _ = line.strip().lstrip('>').partition(' ')
+            contig_id, xbin = line.split(',')
+            xbin = int(xbin)
+        except Exception as e:
+            print('Failed to parse clustering file {}, offending line: {}: Error: '
+                  '{}: {}'.format(args.clustering, line, e.__class__.__name__, e))
+            sys.exit(1)
 
-    try:
-        bin = contigs[contig_id]
-    except KeyError:
-        # contig was not binned for whatever reason
+        contigs[contig_id] = xbin
+        bins.add(xbin)
+    print('done')
+
+    print('  Found {} contigs with bin assignments'.format(len(contigs)))
+    print('  Found {} bins'.format(len(bins)))
+
+    print('Copying contigs...', end='', flush=True)
+    # get output file handles
+    files = {}
+    for i in bins:
+        suffix = args.suffix.lstrip('.')
+        fname = 'bin_{}.{}'.format(i, suffix)
+        files[i] = (outdir / fname).open('w')
+
+    # write contigs
+    contig_id = None
+    num_not_binned = 0
+    for line in args.contigs:
+        is_header = line.startswith('>')
         if is_header:
-            num_not_binned += 1
-        continue
-    else:
-        files[bin].write(line)
+            # account previous contig
+            try:
+                del contigs[contig_id]
+            except KeyError:
+                pass
+            contig_id, _, _ = line.strip().lstrip('>').partition(' ')
 
-# account for last contig
-try:
-    del contigs[contig_id]
-except KeyError:
-    pass
+        try:
+            bin = contigs[contig_id]
+        except KeyError:
+            # contig was not binned for whatever reason
+            if is_header:
+                num_not_binned += 1
+            continue
+        else:
+            files[bin].write(line)
 
-for i in files.values():
-    if i.tell() == 0:
-        print('WARNING: {} is empty'.format(i.name))
-    i.close()
-print('done')
+    # account for last contig
+    try:
+        del contigs[contig_id]
+    except KeyError:
+        pass
 
-if contigs:
-    print('WARNING: {} contigs were clustered but not written out (not found '
-          'in contigs file?)'.format(len(contigs)))
-    if args.verbose:
-        for i, b in contigs.items():
-            print(i, b)
+    for i in files.values():
+        if i.tell() == 0:
+            print('WARNING: {} is empty'.format(i.name))
+        i.close()
+    print('done')
 
-if num_not_binned > 0:
-    print('Found {} contigs without bin assignment'.format(num_not_binned))
+    if contigs:
+        print('WARNING: {} contigs were clustered but not written out (not found '
+              'in contigs file?)'.format(len(contigs)))
+        if args.verbose:
+            for i, b in contigs.items():
+                print(i, b)
+
+    if num_not_binned > 0:
+        print('Found {} contigs without bin assignment'.format(num_not_binned))
