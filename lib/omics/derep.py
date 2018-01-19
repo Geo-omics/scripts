1806734108f60e8ae42eb7cfb4f62a99d2b2af05
Robert <heinro@umich.edu>
2018-01-19 11:21:57 -0500

    [derep] Speed up data loading and replicate searching
      * Do sequence hashing and replicant searching while loading data

--- lib/omics/derep.py
+++ lib/omics/derep.py
@@ -1,5 +1,5 @@
 import argparse
-from collections import defaultdict, namedtuple
+from collections import OrderedDict, namedtuple
 from enum import Enum
 from pathlib import Path
 
@@ -10,24 +10,15 @@ def load(path):
     Read = namedtuple('Read', ['header', 'seq', 'score'])
     State = Enum('State', 'head seq plus score')
     state = State.head
-    data = {}
+    data = OrderedDict()
     cur = {}
-    cur_id = None
+    total_count = 0
     for line in path.open():
         line = line.strip()
         if state is State.head:
             if not line[0] == '@':
                 raise RuntimeError('Expected fastq header: {}'.format(line))
             cur['header'] = line
-            try:
-                cur_id = tuple((
-                    int(i) for i in
-                    line[1:].split()[0].split(':')[4:7]
-                ))
-            except Exception:
-                print('Failed to parse header:', line)
-                raise
-
             state = State.seq
         elif state is State.seq:
             cur['seq'] = line
@@ -38,43 +29,29 @@ def load(path):
             state = State.score
         elif state is State.score:
             cur['score'] = line
-            if cur_id is None or not cur:
-                raise RuntimeError('Bad internal state: {} {}\n{}'
-                                   ''.format(cur_id, cur, line))
+            if not cur:
+                raise RuntimeError('Bad internal state: {}:\n{}'
+                                   ''.format(cur, line))
             if len(cur['seq']) != len(cur['score']):
                 raise RuntimeError('Sanity check failed: sequence and quality '
                                    'score length differ: {}'.format(cur))
-            try:
-                data[cur_id] = Read(**cur)
-            except Exception:
-                print('Failed to constract Read object:', cur_id, '\n', cur)
-                raise
+
+            read = Read(cur['header'], cur['seq'], cur['score'])
+            if cur['seq'] in data:
+                data[cur['seq']].append(read)
             else:
-                cur = {}
-                cur_id = None
-                state = State.head
+                data[cur['seq']] = [read]
+
+            cur = {}
+            state = State.head
+            total_count += 1
 
         else:
             raise RuntimeError('Illegal state reached: {}'.format(state))
 
-    return data
+    return data, total_count
 
 
-def get_duplicates(data):
-    ID_mean = namedtuple('ID_mean', ['id', 'mean_score'])
-    seqs = defaultdict(list)
-    print('Searching for duplicates...', end='', flush=True)
-    for id, read in data.items():
-        mean_score = sum([ord(i) for i in read.score]) / len(read.score)
-        seqs[read.seq].append(ID_mean(id, mean_score))
-
-    print(' done')
-    dups = {}
-    for seq, id_means in seqs.items():
-        if len(id_means) > 1:
-            dups.update(seq=id_means)
-            
-    return dups
 
 
 def main():
@@ -92,14 +69,15 @@ def main():
     fwd_reads = Path(args.forward_reads.name)
     rev_reads = Path(args.reverse_reads.name)
 
-    fwd = load(fwd_reads)
-    print('Loaded', len(fwd), 'reads from', fwd_reads)
+    fwd_data, fwd_total = load(fwd_reads)
+    print('{}: total read count: {}, duplicates: {}'
+          ''.format(fwd_reads, fwd_total, fwd_total - len(fwd_data)))
+    rev_data, rev_total = load(rev_reads)
+    print('{}: total read count: {}, duplicates: {}'
+          ''.format(rev_reads, rev_total, rev_total - len(rev_data)))
     dupes = get_duplicates(fwd)
     print('Found', len(dupes), '(unique) sequences with multiple occurrences')
-    for k,v in list(dupes.items())[:10]:
-        print(k, v)
 
-    #rev = load(args.reverse_reads)
     #print(fwd, rev)
 
 
