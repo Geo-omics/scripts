77078549aa268fae42f73105c42fb74105391382
Robert <heinro@umich.edu>
2018-01-23 11:37:13 -0500

    [derep] Load fwd and rev data at same time and save duplicates

--- lib/omics/derep.py
+++ lib/omics/derep.py
@@ -1,6 +1,4 @@
 import argparse
-from collections import OrderedDict
-from enum import Enum
 from pathlib import Path
 
 from . import get_argparser
@@ -11,52 +9,68 @@ ST_PLUS = 3
 ST_SCORE = 4
 
 
-def load(path):
+def find_duplicates(fwd_path, rev_path=None, *, check=False):
     """
-    Load data from given fastq file
+    Find duplicate reads in given fastq files
+    """
+    if rev_path is None:
+        raise NotImplemented('Single reads processing not implemented')
 
-    Stores data in dictionary, one list of reads per unique sequence.
-    """
+    fwd_f = fwd_path.open('rb')
+    rev_f = rev_path.open('rb')
+
     state = ST_HEAD
-    data = OrderedDict()
-    cur_head = None
-    cur_seq = None
-    cur_score = None
+    data = {}
+    fwd_read_pos = 0
+    rev_read_pos = 0
+    paired_hash = None
     total_count = 0
-    for line in path.open('rb'):
+    for fwd_line, rev_line in zip(fwd_f, rev_f):
         if state is ST_HEAD:
-            if not line[0] == ord('@'):
-                if line[0] == ord('>'):
+            if check:
+                if ord('>') in [fwd_line[0], rev_line[0]]:
                     raise NotImplementedError('Fasta support not implemented')
-                raise RuntimeError('Expected fastq header: {}'.format(line))
-            cur_head = line
+                if fwd_line[0] != ord('@'):
+                    raise RuntimeError('Expected fastq header in {}: {}'
+                                       ''.format(fwd_path, fwd_line))
+                if fwd_line[0] != ord('@'):
+                    raise RuntimeError('Expected fastq header in {}: {}'
+                                       ''.format(fwd_path, fwd_line))
             state = ST_SEQ
         elif state is ST_SEQ:
-            cur_seq = line
+            paired_hash = (fwd_line + rev_line).__hash__()
             state = ST_PLUS
         elif state is ST_PLUS:
-            if not line == b'+\n':
-                raise RuntimeError('Expected "+": {}'.format(line))
+            if check and not fwd_line == rev_line == b'+\n':
+                raise RuntimeError('Expected "+":\n{}\n'
+                                   ''.format(fwd_line, rev_line))
             state = ST_SCORE
         elif state is ST_SCORE:
-            cur_score = line
-            if None in [cur_head, cur_seq]:
-                raise RuntimeError('Bad internal state: {}:\n{}'
-                                   ''.format((cur_head, cur_seq), line))
-            if len(cur_seq) != len(cur_score):
-                raise RuntimeError('Sanity check failed: sequence and quality '
-                                   'score length differ:\n{}\n{}\n'
-                                   ''.format(cur_seq, cur_score))
-
-            read = (cur_head, cur_seq, cur_score)
-            seq_hash = cur_seq.__hash__()
-            if seq_hash in data:
-                data[seq_hash].append(read)
+            cur_mean = mean_quality_score(fwd_line + rev_line)
+            if paired_hash in data:
+                best_mean, pos_list = data[paired_hash]
+                if cur_mean > best_mean:
+                    # new best read goes first
+                    data[paired_hash] = (
+                        cur_mean,
+                        [(fwd_read_pos, rev_read_pos)] + pos_list
+                    )
+                else:
+                    # read to be deleted, goes at end
+                        data[paired_hash] = (
+                            best_mean,
+                            pos_list + [(fwd_read_pos, rev_read_pos)]
+                        )
             else:
-                data[seq_hash] = [read]
+                data[paired_hash] = (
+                    cur_mean,
+                    [(fwd_read_pos, rev_read_pos)]
+                )
 
-            cur = {}
             state = ST_HEAD
+            # set positions for next read
+            fwd_read_pos = fwd_f.tell()
+            rev_read_pos = rev_f.tell()
             total_count += 1
 
         else:
@@ -65,8 +79,11 @@ def load(path):
     return data, total_count
 
 
-def mean_quality_score(read):
-    return sum([ord(i) for i in read.score]) / len(read.score)
+def mean_quality_score(score):
+    """
+    :param bytes score: Byte string encoding the score
+    """
+    return sum(score) / len(score)
 
 
 def unique_pairs(fwd_data, rev_data):
@@ -113,6 +130,11 @@ def main():
     argp.add_argument('forward_reads', type=argparse.FileType())
     argp.add_argument('reverse_reads', type=argparse.FileType())
     argp.add_argument(
+        '-c', '--check',
+        action='store_true',
+        help='Run sanity checks on input'
+    )
+    argp.add_argument(
         '-o', '--out-prefix',
         default='derep_',
         help='Prefix attached to output files',
@@ -122,15 +144,13 @@ def main():
     args.forward_reads.close()
     args.reverse_reads.close()
 
-    fwd_reads = Path(args.forward_reads.name)
-    rev_reads = Path(args.reverse_reads.name)
+    fwd_path = Path(args.forward_reads.name)
+    rev_path = Path(args.reverse_reads.name)
 
-    fwd_data, fwd_total = load(fwd_reads)
-    print('{}: total read count: {}, duplicates: {}'
-          ''.format(fwd_reads, fwd_total, fwd_total - len(fwd_data)))
-    rev_data, rev_total = load(rev_reads)
-    print('{}: total read count: {}, duplicates: {}'
-          ''.format(rev_reads, rev_total, rev_total - len(rev_data)))
+    data, total_reads = find_duplicates(fwd_path, rev_path, check=args.check)
+
+    print('total read count: {}, duplicates: {}'
+          ''.format(total_reads, total_reads - len(data)))
 
     fwd_out = fwd_reads.parent / (args.out_prefix + fwd_reads.name)
     rev_out = rev_reads.parent / (args.out_prefix + rev_reads.name)
