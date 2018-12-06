379f73aea35d89e387102318364f9fdcfb7c57af
Robert <heinro@umich.edu>
2018-12-06 11:10:44 -0500

    [db] Allow calling django's management commands via "omics db"

--- lib/omics/db/__init__.py
+++ lib/omics/db/__init__.py
@@ -1,14 +1,6 @@
 """
 This is the omics database module
 
-This is 'stand-alone' Django ORM and db backend.
+This is 'stand-alone' Django ORM and db backend for the geo-omics-scripts.
 """
-config = {
-    'INSTALLED_APPS': ['omics.db.apps.OmicsDBConfig'],
-    'DATABASES': {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': None,
-        },
-    },
-}
+from .manage import main
--- lib/omics/db/manage.py
+++ lib/omics/db/manage.py
@@ -1,11 +1,45 @@
+import argparse
+from pathlib import Path
 import sys
 
 from django.conf import settings
 from django.core.management import execute_from_command_line
 
-from . import config as db_settings
+
+DEFAULT_DB_FILE = 'omics.db'
+PROG_NAME = 'omics db'  # for argparse and django management cmd benefit
+
+db_settings = {
+    'INSTALLED_APPS': ['omics.db.apps.OmicsDBConfig'],
+    'DATABASES': {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': None,
+        },
+    },
+}
+
+
+argp = argparse.ArgumentParser(add_help=False, prog=PROG_NAME)
+argp.add_argument(
+    '--db',
+    default=DEFAULT_DB_FILE,
+    help='Path and name of database file',
+)
+
+
+def main(argv=None):
+    args, rest_argv = argp.parse_known_args(argv)
+    if not Path(args.db).is_file():
+        print('No database present, file not found: {}\nRun "omics db migrate"'
+              ' to set up a database in the current directory.'
+              ''.format(args.db), file=sys.stderr)
+
+    db_settings['DATABASES']['default']['NAME'] = args.db
+
+    settings.configure(**db_settings)
+    execute_from_command_line([PROG_NAME] + rest_argv)
 
 
 if __name__ == "__main__":
-    settings.configure(**db_settings)
-    execute_from_command_line(sys.argv)
+    main()
