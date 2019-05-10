94c2aa8c5879c581972a0c081f23289039d43166
Robert <heinro@umich.edu>
2019-05-10 12:23:21 -0400

    Remove duplicate file
    File was introduced by "long overdue updates" (6477ccdbcca) but fixing
    up the apropriate "Move all scripts into scripts/" (0aca49fc10c77b) via
    rebase failed to preserve the branch structure

--- wrappers/AntiSmash/antiSmash_summary.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-echo "GeneClusters: $(grep -v "^>" Overview.geneclusters.txt | wc -l)"
-echo "smcogs: $(grep -c "^>>" Overview.smcogs.txt)"
-
-ls */structures/* | cut -f 1 -d "/" | sort -u > structures.list
-echo "Structures: `wc -l structures.list`"
